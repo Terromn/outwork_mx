de286055123aa2058b9a9ba75e5867d61e5ab5dd
Terromn <neil.rodriguez.murillo@gmail.com>
2023-07-08 17:58:40 -0600

    mobile menu done

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+const menuButton = document.getElementById('menuButton');
+const mobileMenu = document.getElementById('mobileMenu');
+const menuItems = mobileMenu.getElementsByTagName('a');
+
+menuButton.addEventListener('click', function() {
+  mobileMenu.classList.toggle('show');
+});
+
+// Add event listeners to menu items
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('click', function() {
+    mobileMenu.classList.remove('show');
+  });
+}
