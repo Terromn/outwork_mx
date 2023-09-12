f4f705b1552deabb37bccd6c6a91460e3a6e810f
Neil Terromn <terromn@MacBook-Air-de-Neil.local>
2023-09-11 22:31:01 -0600

    Still missing: Sucess payment buttons for go to app, or got to home firebase login on website if not login, you cant buy shit :D

--- public/js/stripe/checkoutSessionChecker.js
+++ public/js/stripe/checkoutSessionChecker.js
@@ -1,21 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-
+document.addEventListener("DOMContentLoaded", async function () {
+    console.log('Console Log Working');
 
     const urlParams = new URLSearchParams(window.location.search);
     const sessionId = urlParams.get('checkout_session_id');
-
-    const stripeKey = Stripe('pk_test_51NoaOoBdpz1dvuej41NlgXot6L4DSOqfz0VXoyNy1XGX692w7yFtIftQ5Cn5hP3zokqzZr2ZdT6koww0ZqVprtW200Hukpti8i');
-
-    stripeKey.checkout.sessions.retrieve(sessionId)
-        .then(function (session) {
-            if (session.payment_status === 'paid') {
-                console.log('Payment was successful');
-            } else {
-                console.log('Payment was not successful');
-            }
-        })
-        .catch(function (error) {
-            console.error('Error fetching session:', error);
-        });
-
+    const credits = urlParams.get('crds');
+    
 });
