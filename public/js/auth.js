f116eb1457bb1857dbbcdb1557f8168ca2e0e5ac
Neil Terromn <terromn@MacBook-Air-de-Neil.local>
2023-09-19 09:42:39 -0600

    added auth

---
+++ public/js/auth.js
@@ -0,0 +1,66 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+
+const firebaseConfig = { apiKey: "AIzaSyCExcWv1RxenkllDOiERix1EoN66ZUh-J4", authDomain: "outwork-f8f3f.firebaseapp.com", databaseURL: "https://outwork-f8f3f-default-rtdb.firebaseio.com", projectId: "outwork-f8f3f", storageBucket: "outwork-f8f3f.appspot.com", messagingSenderId: "296052478452", appId: "1:296052478452:web:ade9633ec13ce6b59029a1", measurementId: "G-CVLBGXKKLW" };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const userEmail = document.getElementById("userEmail");
+    const userPassword = document.getElementById("userPassword");
+    const signInButton = document.getElementById("signInButton");
+    const signUpButton = document.getElementById("signUpButton");
+
+    const userSignUp = async () => {
+        const signUpEmail = userEmail.value;
+        const signUpPassword = userPassword.value;
+
+        createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                window.location.href = "../index.html";
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode + errorMessage);
+            });
+    };
+
+    const userSignIn = async () => {
+        const signInEmail = userEmail.value;
+        const signInPassword = userPassword.value;
+
+        signInWithEmailAndPassword(auth, signInEmail, signInPassword)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                window.location.href = "../index.html";
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode + errorMessage);
+            });
+    };
+
+    signUpButton.addEventListener("click", userSignUp);
+    signInButton.addEventListener("click", userSignIn);
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log("User is authenticated.");
+        } else {
+            console.log("User is not authenticated.");
+        }
+    });
+});
--- public/js/stripe/checkoutSessionChecker.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", async function () {
-    console.log('Console Log Working');
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const sessionId = urlParams.get('checkout_session_id');
-    const credits = urlParams.get('crds');
-    
-});
