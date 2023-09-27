92f58df57fe3788c6b8875fe4e6434b5b01a6441
Neil Terromn <terromn@MacBook-Air-de-Neil.local>
2023-09-26 21:07:02 -0600

    auth working, uplpoad to database stopped working

--- public/js/htmls/afterPayment.js
+++ public/js/htmls/afterPayment.js
@@ -1,23 +1,27 @@
-import { 
-    auth, 
+import {
+    auth,
 
     db,
     doc,
-    setDoc, 
-} from './firebase.js';
+    setDoc,
+} from '../firebase.js';
 
 const currentUrl = window.location.href;
 const urlParams = new URLSearchParams(currentUrl);
 const crds = urlParams.get("crds");
 
-document.addEventListener("DOMContentLoaded",  function () {
+document.addEventListener("DOMContentLoaded", function () {
+
     console.log("DOMContentLoaded event fired.");
-        const uid = auth.currentUser.uid;
 
-        console.log("starting to upload the credits...s")
-          setDoc(doc(db, "users", uid), {
-            creditsAvailable: crds,
-        })
+    const uid = auth.currentUser.uid;
 
+    console.log("starting to upload the credits...s");
+
+    setDoc(doc(db, "users", uid), {
+
+        creditsAvailable: crds,
+    
+    })
 
 });
