e79ebaf6a24da726f5630b733f55d1937da04f78
Neil Terromn <neil.rodriguez.murillo@gmail.com>
2023-10-02 11:13:25 -0600

    everything working correctly

--- public/js/htmls/index.js
+++ public/js/htmls/index.js
@@ -1,22 +1,25 @@
 import { isLoggedIn, logoutUser } from '../auth.js';
 
-AOS.init({
-  duration: 1200,
-})
-
 
 const buttons = document.getElementsByClassName("btn-price");
-const btnLogText = document.getElementById("btn-log-text");
-const btnLog = document.getElementById("btn-log");
+const btnLogTexts = document.getElementsByClassName("btn-log-text");
+const btnLogs = document.getElementsByClassName("btn-log");
 
 const buttonArray = Array.from(buttons);
+
+const btnLogTextsArray = Array.from(btnLogTexts);
+const btnLogsArray = Array.from(btnLogs);
 
 const updateLoginButtonText = () => {
     isLoggedIn().then((loggedIn) => {
         if (loggedIn) {
-            btnLogText.textContent = "LOGOUT";
+            btnLogTextsArray.forEach((btnLogText) => {
+                btnLogText.textContent = "LOGOUT";
+            })
         } else {
-            btnLogText.textContent = "LOGIN";
+            btnLogTextsArray.forEach((btnLogText) => {
+                btnLogTexts.textContent = "LOGIN";
+            })
         }
     });
 };
@@ -26,28 +29,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
     updateLoginButtonText();
 
-    btnLog.addEventListener("click", async (event) => {
-        event.preventDefault();
+    btnLogsArray.forEach((btnLogs) => {
+        btnLogs.addEventListener("click", async (event) => {
+            event.preventDefault();
 
-        const link = btnLog.getAttribute("data-link");
-        const isAuthenticated = await isLoggedIn();
+            const link = btnLogs.getAttribute("data-link");
+            const isAuthenticated = await isLoggedIn();
 
-        if (isAuthenticated) {
-            btnLogText.textContent = "LOGIN";
+            if (isAuthenticated) {
+                btnLogTexts.textContent = "LOGOUT";
 
-            logoutUser()
-                .then(() => {
-                    console.log("User has been logged out.");
-                })
-                .catch((error) => {
-                    console.error("Error logging out:", error);
-                });
-        } else {
-            console.log("Redirecting to:", link);
-            btnLogText.textContent = "LOGOUT";
-            window.location.href = link;
-        }
-    });
+                logoutUser()
+                    .then(() => {
+                        console.log("User has been logged out.");
+                    })
+                    .catch((error) => {
+                        console.error("Error logging out:", error);
+                    });
+            } else {
+                console.log("Redirecting to:", link);
+                btnLogTexts.textContent = "LOGIN";
+                window.location.href = link;
+            }
+        });
+    })
+
 
 
     buttonArray.forEach((button) => {
@@ -55,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
             event.preventDefault();
 
             const link = button.getAttribute("data-link");
+            console.log(link);
             const isAuthenticated = await isLoggedIn();
 
             if (isAuthenticated) {
