65b86e394ccd438da4c6b41292aecf045ed6865b
Neil Terromn <neil.rodriguez.murillo@gmail.com>
2023-10-10 10:50:41 -0600

    fixed the login page and remove auto complete feature

--- public/js/htmls/loginAndRegister.js
+++ public/js/htmls/loginAndRegister.js
@@ -10,21 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     signInButton.addEventListener("click", function () {
         // Add the "btn-green" class and remove the "btn-black" class from the Sign In button.
-        const currentAutocompleteValue = passwordField.getAttribute("autocomplete");
-        if (currentAutocompleteValue === "new-password") {
-            // If it's "new-password," change it to "password"
-            passwordInput.setAttribute("autocomplete", "password");
-            emailInput.setAttribute("autocomplete", "username");
-        };
-
         signInButton.classList.add("btn-green");
         signInButton.querySelector("p").classList.add("text-invert-color");
         signInButton.classList.remove("btn-black");
 
         nameInput.classList.add("d-none");
         nameInput.classList.remove("d-block");
-
-
 
         // Add the "btn-black" class and remove the "btn-green" class from the Sign Up button.
         signUpButton.classList.add("btn-black");
@@ -38,12 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     signUpButton.addEventListener("click", function () {
-        const currentAutocompleteValue = passwordField.getAttribute("autocomplete");
-        if (currentAutocompleteValue === "password") {
-            // If it's "new-password," change it to "password"
-            passwordField.setAttribute("autocomplete", "new-password");
-            emailInput.setAttribute("autocomplete", "new-username");
-        };
+        // Add the "btn-green" class and remove the "btn-black" class from the Sign In button.
+
         // Add the "btn-green" class and remove the "btn-black" class from the Sign In button.
         signInButton.classList.remove("btn-green");
         signInButton.querySelector("p").classList.remove("text-invert-color");
@@ -52,7 +39,6 @@ document.addEventListener("DOMContentLoaded", function () {
         nameInput.classList.add("d-block");
         nameInput.classList.remove("d-none");
 
-
         // Add the "btn-black" class and remove the "btn-green" class from the Sign Up button.
         signUpButton.classList.remove("btn-black");
         signUpButton.querySelector("p").classList.add("text-invert-color");
@@ -60,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Move the Sign In button above the Sign Up button in the DOM.
         signUpButton.parentNode.insertBefore(signUpButton, signInButton);
-
 
     });
 
